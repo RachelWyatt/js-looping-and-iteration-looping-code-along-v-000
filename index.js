9ea43d3566c7e0a9001b431b44eb7e774ec7ff20
Rachel Wyatt <rw126@evansville.edu>
2019-04-24 18:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Welcome ${names[i]}! You are employee number ${[i]}!`);
+    }
+ 
+    return names;
+}
+
+function tailsNeverFails() {
+    return Math.random() >= 0.5;
+    
+}
+ 
+while (tailsNeverFails()) {
+  
+    console.log('You got X tails in a row!');
+}
